4e9ade63a2dfd53e61b119e041af42fe3858992d
jangwon <wkddnjset@gmail.com>
2018-10-31 03:37:51 +0900

    퍼블리싱 진행중~

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 950, height: 680})
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
